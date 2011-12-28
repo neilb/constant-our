5f801bcc0d7c2d6d2b81a8766b0c7979ff571a99
Neil Bowers <neil@bowers.com>
2011-12-28 09:44:37 +0000

    Added pod documentation to constant::our::ENV to stop tests failing. If you have Pod::Coverage and Test::Pod::Coverage installed, then "Build test" would fail, because there was no pod for the module.

--- lib/constant/our/ENV.pm
+++ lib/constant/our/ENV.pm
@@ -3,6 +3,8 @@ package constant::our::ENV;
 use warnings;
 use strict;
 
+our $VERSION = '0.02';
+
 my %constants;
 BEGIN
 {
@@ -16,3 +18,38 @@ BEGIN
 }
 use constant::our \%constants;
 1;
+
+=head1 NAME
+
+constant::our::ENV - define constants based on environment variables
+
+=head1 SYNOPSIS
+
+  BEGIN {
+      $ENV{CONSTANT_OUR_DEBUG} = 1;
+  }
+  use constant::our::ENV;
+  
+  print "Running in debug mode\n" if DEBUG;
+
+=head1 DESCRIPTION
+
+This module looks for any environment variables whose name starts with
+CONSTANT_OUR_, and defines constants using L<constant::our> for each
+such variable.
+
+=head1 SEE ALSO
+
+L<constant::our>
+
+=head1 AUTHOR
+
+Green, E<lt>Evdokimov.Denis at gmail.comE<gt>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Green, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
